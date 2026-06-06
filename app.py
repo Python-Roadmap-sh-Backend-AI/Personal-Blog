cdfa59afb228f8b4215d5a35323df891c9caf037
Gamidu Welikanna <gamidukaveen@gmail.com>
2026-06-06 16:29:19 +0530

    feat: added the code for login, logout and for admin user

--- app.py
+++ app.py
@@ -1,7 +1,13 @@
-from flask import Flask, render_template, request
+from flask import Flask, redirect, render_template, request, session
 
 from utilis.article_manager import get_articles
 app = Flask(__name__) 
+app.secret_key = "mysecreykey"
+
+USERNAME = "admin"
+PASSWORD = "12345"
+
+
 
 @app.route('/')
 def home():
@@ -18,6 +24,32 @@ def article(filename):
 
     return render_template('article.html', content=html_content)
 
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+
+        if username == USERNAME and password == PASSWORD:
+            session["admin"] = True
+            return redirect("/dashboard")
+        
+        return "Invalid Credentials"
+    return render_template('login.html')
+
+@app.route('/dashboard')
+def dashboard():
+    if not session.get("admin"):
+        return redirect("/login")
+    
+    articles = get_articles()
+    return render_template('dashboard.html', articles=articles)
+
+
+@app.route('/logout')
+def logout():
+    session.pop("admin", None)
+    return redirect("/login")
 
 
 if __name__ == '__main__':
